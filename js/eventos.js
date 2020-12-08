3515a35fe538d000c13a022f892a51f03927b984
gonzalo-marin <gonzalo.marn@gmail.com>
2020-12-08 18:52:38 +0100

    Botón eliminar

--- js/eventos.js
+++ js/eventos.js
@@ -2,6 +2,7 @@ let nuevaTarea = document.querySelector('#tarea');
 let prioridad = document.querySelector('#prioridad');
 let guardar = document.querySelector('#btnGuardar');
 let seccion = document.querySelector('.tareas');
-//let id = 1;
+let id = 1;
 
 guardar.addEventListener('click', recogerDatos);
+
--- js/funciones.js
+++ js/funciones.js
@@ -1,17 +1,3 @@
-/* function guardarTarea(event) {
-    event.preventDefault();
-    let inputTarea = nuevaTarea.value.trim();
-    let inputPrioridad = prioridad.value.trim();
-    console.log(nuevaTarea.value, nuevaPrioridad.value);
-
-    if (inputTarea != "" && inputPrioridad != -1 && inputPrioridad != "") {
-        agregarTarea(inputTarea, inputPrioridad);
-    } else {
-        alert('¡Los campos no pueden estar vacíos!');
-    }
-    nuevaTarea.value = "";
-    nuevaPrioridad.value = "";
-} */
 
 function recogerDatos(event) {
     event.preventDefault();
@@ -34,17 +20,19 @@ function recogerDatos(event) {
 
 function guardarTarea(pNuevaTarea, pPrioridad) {
     let tarea = {
+        id: id,
         nombre: pNuevaTarea,
         prioridad: pPrioridad
     }
 
     let tareaRepetida = arrTareas.findIndex(tarea => {
-        return tarea.nombre == pNuevaTarea | tarea.prioridad == pPrioridad
+        return tarea.nombre == pNuevaTarea /* && tarea.prioridad == pPrioridad */
     })
 
     if (tareaRepetida == -1) {
         arrTareas.push(tarea);
         mostrarTarea(pNuevaTarea, pPrioridad, seccion);
+        id++;
         console.log(arrTareas);
     } else {
         alert('Tarea ya guardada!!')
@@ -53,25 +41,31 @@ function guardarTarea(pNuevaTarea, pPrioridad) {
 
 function mostrarTarea(pNuevaTarea, pPrioridad, pSeccion) {
     let li = document.createElement('li');
-    let contenidoli = document.createTextNode(`${pNuevaTarea}: ${pPrioridad}`);
+    let contenidoli = document.createTextNode(`${pNuevaTarea}: ${pPrioridad} `);
     li.appendChild(contenidoli);
+
+    let a = document.createElement('a');
+    a.addEventListener('click', eliminarTarea);
+    a.innerText = "Eliminar";
+    a.href = "#";
+    a.dataset.id = id;
+
+    li.appendChild(a);
     pSeccion.appendChild(li);
 }
 
-/* function agregarTarea(pNuevaTarea, pNuevaPrioridad) {
-    let tarea = {
-        id = id,
-        titulo = pNuevaTarea,
-        prioridad = pNuevaPrioridad
-    }
-    let repetido = arrTareas.findIndex(tarea => {
-        return tarea.titulo = pNuevaTarea;
-    });
-    if (repetido == -1) {
-        arrTareas.push(tarea);
-        id++;
-        console.log(arrTareas);
-    } else {
-        alert('Tarea ya anotada');
-    }
-} */
+function eliminarTarea(event) {
+    event.preventDefault();
+    //console.log(event.target.parentNode);
+    //event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+    let tareaHecha = event.target.parentNode;
+    tareaHecha.parentNode.removeChild(tareaHecha);
+
+    let idBorrar = event.target.dataset.id;
+    //console.log(idBorrar);
+
+    let posicion = arrTareas.findIndex(tarea => tarea.id == idBorrar);
+    arrTareas.splice(posicion, 1);
+    console.log(arrTareas);
+}
+
